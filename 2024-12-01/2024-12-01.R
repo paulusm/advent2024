70a06360bed53629c0cbffd8d98153f10ea77d2d
Paul Matthews <paul2.matthews@uwe.ac.uk>
2024-12-01 12:05:00 +0000

    tidy up

--- 2024-12-01/2024-12-01.R
+++ 2024-12-01/2024-12-01.R
@@ -1,27 +1,17 @@
-# Part One
-# Test
-left <- c(3,4,2,1,3,3) |> sort()
-right <- c(4,3,5,3,9,3) |> sort()
+library(readr)
+library(dplyr)
+library(tidyr)
 
-input <- read_table("2024-12-01/input.txt", col_names = FALSE)
+# Part One
+# input <- read_table("2024-12-01/test.txt", col_names = FALSE)
+input <- read_table("2024-12-01/input.txt", col_names = FALSE, show_col_types = FALSE)
 left <- input$X1 |> sort(method = "radix")
 right <- input$X2 |> sort(method = "radix")
 answer <- sum(abs(left - right))
 print(answer)
 
 # Part Two
-# Test
-library(purrr)
-library(dplyr)
-
-testdf <- tibble(
-left = c(3,4,2,1,3,3),
-right = c(4,3,5,3,9,3)
-)
-testdf <- testdf |> mutate(count = map(left, \(x) testdf |> filter(right == x) |> nrow()) )
-str(testdf)
-print(sum(as.numeric(testdf$count) * testdf$left))
-
-input <- input |> mutate(count = map(X1, \(x) input |> filter(X2 == x) |> nrow() ) )
-answer2 <- sum(as.numeric(input$count) * input$X1)
-print(answer2)
+freqs <- table(input$X2)
+input$freq <- freqs[as.character(input$X1)] |> as.double()
+answer <- sum(input$X1 * input$freq |> replace_na(0))
+print(answer)
