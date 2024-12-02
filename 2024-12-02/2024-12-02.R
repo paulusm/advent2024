5704e0742f95de09f43ab81940930cfae5e7f669
Paul Matthews <paul2.matthews@uwe.ac.uk>
2024-12-02 18:51:07 +0000

    its slow but its not far

--- 2024-12-02/2024-12-02.R
+++ 2024-12-02/2024-12-02.R
@@ -1,11 +1,21 @@
 library(readr)
 library(purrr)
+library(dplyr)
+library(tidyr)
 
-input <- read_table("2024-12-02/test.txt", col_names = FALSE, show_col_types = FALSE)
+input <- read_table("2024-12-02/input.txt", col_names = FALSE, show_col_types = FALSE, )
 
-input |> group_by(row_number()) |>
-    group_map(\(x) {
-        left <- x$X1 |> sort(method = "radix")
-        right <- x$X2 |> sort(method = "radix")
-        sum(abs(left - right))
-    }) |> print()
+
+resultList <- input |> group_by(row_number()) |>
+    group_map(\(x, i) {
+        x[!is.na(x)] |> reduce2(c(x[3:length(x)],999), \(x, y, z){
+
+            if(z == 999) return(done(TRUE))
+            if(abs(x - y) > 3){return(done(FALSE))}
+            if( x == y) {return(done(FALSE))}
+            if((y > x & z < y) | (y < x & z > y)) {return(done(FALSE))}
+            return (y)
+        })
+    })
+
+resultList[resultList == TRUE] |> length() |> print()
