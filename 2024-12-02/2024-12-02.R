5a906dc7701101b2efe2b678a3be8a5b23632e87
Paul Matthews <paul2.matthews@uwe.ac.uk>
2024-12-03 17:24:18 +0000

    try pt 2

--- 2024-12-02/2024-12-02.R
+++ 2024-12-02/2024-12-02.R
@@ -25,5 +25,39 @@ resultList <- input |> group_by(row_number()) |>
 input$safe <- resultList
 resultList[resultList == TRUE] |> length() |> print()
 
-# Part Two - Not done
+# Part Two, TODO: Refactor!
+# 278 is too low
+input <- read_table("2024-12-02/input.txt", col_names = FALSE, show_col_types = FALSE, )
 
+resultList <- input |> group_by(row_number()) |>
+    group_map(\(x, i) {
+        x <- x[!is.na(x)]
+        #print(x)
+        parse1 <- x |> reduce2(c(x[3:length(x)],999), \(x, y, z, pos = 0){
+            pos <- pos + 1
+            print(pos)
+            if(abs(x - y) > 3){return(done(list(FALSE, pos)) )}
+            if( x == y) {return(done(list(FALSE, pos)))}
+            if(z == 999) return(done(list(TRUE, pos)))
+            if((y > x & z < y) | (y < x & z > y)) {return(done(list(FALSE, pos)))}
+            return (y)
+        })
+
+        if(parse1[[1]] == FALSE){
+            pos <- parse1[[2]]
+            print(pos)
+            x <- x[-pos]
+            print(x)
+            parse2 <<- x |> reduce2(c(x[3:length(x)],999), \(x, y, z, pos = 0){
+                if(abs(x - y) > 3){return(done(list(FALSE, pos)) )}
+                if( x == y) {return(done(list(FALSE, pos)))}
+                if(z == 999) return(done(list(TRUE, pos)))
+                if((y > x & z < y) | (y < x & z > y)) {return(done(list(FALSE, pos)))}
+                return (y)
+            })
+        }
+        return(parse1[[1]] | parse2[[1]])
+    })
+
+input$safe <- resultList
+resultList[resultList == TRUE] |> length() |> print()
