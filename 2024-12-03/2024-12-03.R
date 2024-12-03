b0549123f37bba2751a2a34359e8269b611180a7
Paul Matthews <paul2.matthews@uwe.ac.uk>
2024-12-03 12:57:49 +0000

    fixed part 2

--- 2024-12-03/2024-12-03.R
+++ 2024-12-03/2024-12-03.R
@@ -1,6 +1,7 @@
 library(readr)
 library(stringr)
 library(purrr)
+library(tibble)
 
 program <- read_file("2024-12-03/input.txt")
 
@@ -8,17 +9,16 @@ program <- read_file("2024-12-03/input.txt")
 matches <- program |> str_match_all("mul\\((\\d+),(\\d+)\\)")
 print(sum(as.numeric(matches[[1]][,2]) * as.numeric(matches[[1]][,3])))
 
-# Part Two, Not working Nasty!
-# 1143376 too low
-# 56375767 too low
+# Part Two, This could be much shorter
 program2 <- read_file("2024-12-03/input.txt")
 dontsplit <- str_split(program2, "don't\\(\\)")
 goodparts <- dontsplit[[1]][1]
 badparts <- dontsplit[[1]][-1]
+view(badparts)
 dosplit <- str_split(badparts, "do\\(\\)")
-moregoodparts <- dosplit |> map(\(x) x[-1])
+moregoodparts <- dosplit |> map(\(x) return(x[-1])) |> reduce(\(x, y) return(c(x, y)) )
 goodparts <- c(goodparts, moregoodparts)
-print(goodparts)
+view(goodparts)
 goodparts |> map(\(x) {
     if(length(x) == 0) return(0)
     matches <- x |> str_match_all("mul\\((\\d+),(\\d+)\\)")
