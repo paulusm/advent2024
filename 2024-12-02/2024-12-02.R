25e348a7e000944d5bc13628747fd610bee385b8
Paul Matthews <paul2.matthews@uwe.ac.uk>
2024-12-05 17:26:17 +0000

    blat 2 start 5

--- 2024-12-02/2024-12-02.R
+++ 2024-12-02/2024-12-02.R
@@ -12,47 +12,4 @@ input <- read_table("2024-12-02/input.txt", col_names = FALSE, show_col_types = 
 
 #242
 
-parse <- function(row){
-    row <- row[!is.na(row)]
-    row <- as.list(row)
-    names(row) <- NULL
-    unsafe <- c()
-    parseResult <- (row |> imap( \(x, idx){
-        if(idx == length(row)) return( idx)
-        y <-  row[[idx + 1]]
-        if(idx > 1) z <-  row[[idx - 1]] else z = NA
-        if(abs(x - y) > 3) unsafe <<- c(unsafe,idx + 1)
-        if( x == y) unsafe <<- c(unsafe,idx + 1)
-        if(idx > 1){
-           if((y > x & z > x) | (y < x & z < x)) unsafe <<- c(unsafe,idx)
-        }
-        return(idx)
-    }))
-    return(unsafe)
-}
-
-#print(parse(input[4,]))
-
-resultList <- input |> group_by(row_number()) |>
-    group_map(\(x, i) {
-        result <- (parse(x))
-        return(length(result) == 0)
-    })
-
-
-input$safe <- resultList
-resultList[resultList == TRUE] |> length() |> print()
-
-# Part Two, not working
-# 278 is too low, 316 is too high!
-input <- read_table("2024-12-02/input.txt", col_names = FALSE, show_col_types = FALSE, )
-
-resultList <- input |> group_by(row_number()) |>
-    group_map(\(x, i) {
-        result <- (parse(x))
-        return(length(result) < 2)
-    })
-
-
-input$safe <- resultList
-resultList[resultList == TRUE] |> length() |> print()
+# Part Two
--- 2024-12-04/2024-12-04.R
+++ 2024-12-04/2024-12-04.R
@@ -1,9 +1,37 @@
 library(readr)
 library(stringr)
 library(purrr)
+library(omnibus)
 
 input <- read_table("2024-12-04/test.txt", col_names = F) |>
-    map(\(x) strsplit(x, split="")) |>
+    map(\(x) strsplit(x, split = "")) |>
     unlist() |> matrix(nrow = 10, byrow = T)
 
+countXmas <- function(mat){
+    return(mat |> as_tibble() |>
+    unite("line", sep = "", na.rm = T) |> str_match_all("(?=(XMAS|SAMX))") |> pluck(1) |> nrow())
+}
+
+resizeRotate45 <- function(mat){
+    # For diagonals, we need to resize the matrix first, then transform it
+    transformed <- matrix(0, nrow = nrow(mat) * 2 + 1 , ncol = ncol(mat) * 2 + 1)
+    position <- nrow(mat) / 2
+    transformed[position:(nrow(input) + position - 1), position:(ncol(input) + position - 1)] <- mat
+    rotated <- matrix(0, nrow = nrow(transformed) , ncol = ncol(transformed) )
+    for (i in 1:(nrow(transformed) - 1))
+        for (j in 1:(ncol(transformed) - 1))
+            print(c(i + j + 1, -i + j + 10))
+            rotated[i + j + 1, -i + j + 10] <- transformed[i, j]
+
+    return(rotated)
+}
+foo <- input |> resizeRotate45()
+
+horizontal <- input |> countXmas()
+vertical <- input |> rotateMatrix(90) |> countXmas()
+
+diagonal1 <- resized |> rotateMatrix(45) |> countXmas()
+diagonal2 <- resized |> rotateMatrix(-45) |> countXmas()
+
+c(horizontal, vertical, diagonal1, diagonal2)
 
