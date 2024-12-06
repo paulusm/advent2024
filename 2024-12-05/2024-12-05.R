c19dbcd2b15f38c7001eb7aef578884c58dbdeab
Paul Matthews <paul2.matthews@uwe.ac.uk>
2024-12-06 18:06:29 +0000

    That was quite painful

--- 2024-12-05/2024-12-05.R
+++ 2024-12-05/2024-12-05.R
@@ -7,24 +7,59 @@ pages <- input[[1]][2] |> str_split("\n") |> pluck(1)
 
 regexes <- rules |> map(\(x) paste0(".*", (x |>
                     str_split("\\|") |> pluck(1) |> str_flatten(collapse = ".*")),".*"))
-pages |> map(\(x) {
-    compliant <- T
-    rules |> imap(\(y, i) {
-        numbers <- y |> str_split("\\|") |> pluck(1)
-        if (grepl(numbers[1], x) & grepl(numbers[2], x) )
-        {
-            # rule match
-            if (x |> str_detect(regexes[[i]])){
-                # rule adhered
+
+getCompliance <- function(pagesToTest, rulesList){
+    compliantPages <- c()
+    noncompliantPages <- c()
+    rulesthatapply <- list()
+    pagesToTest |> imap(\(x, pagenum) {
+        compliant <- T
+        rulesList |> imap(\(y, i) {
+            numbers <- y |> str_split("\\|") |> pluck(1)
+            if (grepl(numbers[1], x) & grepl(numbers[2], x) )
+            {
+                if (!(x |> str_detect(regexes[[i]]))) {
+                    # rule adhered
+                    compliant <<- F
+                    # rule match
+                    rulesthatapply[[x]] <<- c(rulesthatapply[[x]], i)
+                }
             }
-            else{
-                # rule broken
-                compliant <<- F
-            }
+        })
+        if (compliant){
+            compliantPages <<- c(compliantPages, x)
+        }
+        else{
+            noncompliantPages <<- c(noncompliantPages, x)
         }
     })
-    if(compliant)
-        return((x |> substring(str_length(x)/2,str_length(x)/2+1)))
-    else
-        return(0)
-}) |> as.numeric() |> sum() |> print()
+    return(list(pass = compliantPages, fail = noncompliantPages, rulesFailed = rulesthatapply))
+}
+
+results <- getCompliance(pages, rules)
+
+# Part One
+results$pass  |>  map(\(x) x |> substring(str_length(x)/2,str_length(x)/2 + 1)) |>
+                      as.numeric() |> sum() |> print()
+
+
+# Part Two - Reordering can lead to new rules to be broken! Doh!
+reOrderResults <- function(failedPages, rulesFailed){
+    reorderedPages <- failedPages |> map(\(x) {
+        reordered <- x
+        rulesFailed[[x]] |> map(\(y) {
+            reordered <<- reordered |> str_replace(rules[y] |> substr(1,2), rules[y] |> substr(4,5))
+            reordered <<- reordered |> str_replace(rules[y] |> substr(4,5), rules[y] |> substr(1,2))
+        })
+        return(reordered )
+    })
+    testReordered <- getCompliance(reorderedPages, rules)
+    if (length(testReordered$fail) > 0) {
+        reReordered <- reOrderResults(testReordered$fail, testReordered$rulesFailed)
+        return(c(reReordered, testReordered$pass))
+    }
+    return(reorderedPages)
+}
+
+reorderedResult <- reOrderResults(results$fail, results$rulesFailed)
+reorderedResult |> map(\(x) x |> substring(str_length(x)/2,str_length(x)/2 + 1)) |> as.numeric() |> sum() |> print()
