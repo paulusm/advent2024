2f9999a8f181e5177b7a713841f5775831c817ff
Paul Matthews <paul2.matthews@uwe.ac.uk>
2024-12-08 15:58:45 +0000

    Faf

--- 2024-12-07/2024-12-07.R
+++ 2024-12-07/2024-12-07.R
@@ -24,19 +24,25 @@ input |> map(\(x) {
      combi  |>  map(\(x) if (x == target) x else 0) |> reduce(max)
 }) |> reduce(sum) |> format(scientific = F)
 
-# Part Two - Not working (?test example wrong 7290 I get 7740?)
+# Part Two - Not working YET
 input[5] |> map(\(x) {
     target <- as.numeric(x[1])
     args <- length(x) - 2
-    ops <- c("+","*","")
+    ops <- c("+","*","|")
     options <- permutations(3, args, ops, repeats.allowed = TRUE) |> t() |> as.tibble()
     combi <- options |> map(\(y){
         results <- x[2:length(x)] |> imap(\(z, i){
-            paste0(z, ifelse(i > args,"",y[i]) )
+            paste0(z, ifelse(i > args,"",y[i]))
         }) |> str_flatten(collapse = "")
-        #results <- paste0(strrep("(", args + 1) ,results)
-        print(results)
-        eval(parse(text = results)) |> as.numeric()
+    part <- results |> str_extract("\\d[*+|]\\d")
+    print(part)
+    #while (!is.na(part)) {
+    results <- results |> str_replace(part, eval(parse(text = part|> str_replace("\\|",""))) |> as.character())
+    print(results)
+    part <- results |> str_extract("\\d[*+|]\\d") |> str_replace("\\|","")
+    print(part)
+    #}
+    results |> as.numeric()
     })
     combi  #|>  map(\(x) if (x == target) x else 0) |> reduce(max)
 }) #|> reduce(sum) |> format(scientific = F)
