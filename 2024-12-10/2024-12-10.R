17cc171339d4cd9e0b1ae18bf7ab5b79129f9fcd
Paul Matthews <paul2.matthews@uwe.ac.uk>
2024-12-10 17:20:04 +0000

    xfer

--- 2024-12-10/2024-12-10.R
+++ 2024-12-10/2024-12-10.R
@@ -1,11 +1,14 @@
 library(readr)
-library(purrr)
+library(furrr)
 library(dplyr)
 library(stringr)
 library(tidyr)
+plan(multiprocess)
 
 landscape <- read_file("2024-12-10/test.txt") |> str_split("\r\n") |> unlist() |> str_split("") |> as_tibble( .name_repair = "unique")
 # convert to three column tibble of x, y, value
 landscape <- landscape |> mutate(y = as.character(row_number())) |>
     pivot_longer(cols = ...1:...8, names_to = "x", values_to = "value")
 landscape$x <- landscape$x |> str_replace("\\.\\.\\.","")
+
+heads <- landscape |> filter(value == "0")
